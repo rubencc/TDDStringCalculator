3cf48024b7475a20e43c37f1fddf8495b195c923
Rubén C.C <ruben.carretero@wolterskluwer.com>
2020-09-22 10:38:30 +0200

    added cases

--- StringCalculator.Implementations/StringCalculator.cs
+++ StringCalculator.Implementations/StringCalculator.cs
@@ -1,15 +1,25 @@
 ﻿namespace StringCalculator.Implementations
 {
     using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
 
     public class StringCalculator
     {
+
         public float Add(string values)
         {
-            if (values == String.Empty)
+            if (String.IsNullOrEmpty(values))
                 return 0;
 
-            return 0;
+            var regex = new Regex(@"^[1-9 ]+$");
+
+            if (!regex.IsMatch(values))
+                return 0;
+            
+            var list = values.Split(' ').Select(float.Parse);
+
+            return list.Sum();
         }
     }
 }
--- TDD/StringCalculatorTests.cs
+++ TDD/StringCalculatorTests.cs
@@ -16,5 +16,45 @@ namespace TDD
 
             result.Should().Be(0);
         }
+
+        [Fact]
+        public void Input_null()
+        {
+            StringCalculator sut = new StringCalculator();
+
+            float result = sut.Add(null);
+
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void Input_not_numbers()
+        {
+            StringCalculator sut = new StringCalculator();
+
+            float result = sut.Add("String.Empty");
+
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void Input_1_2()
+        {
+            StringCalculator sut = new StringCalculator();
+
+            float result = sut.Add("1 2");
+
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Input_4_4_5()
+        {
+            StringCalculator sut = new StringCalculator();
+
+            float result = sut.Add("4 4 5");
+
+            result.Should().Be(13);
+        }
     }
 }
