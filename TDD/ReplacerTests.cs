0389989d255a502135e85f3cd4763c68a33e3f4b
Rubén C.C <ruben.carretero@wolterskluwer.com>
2020-09-22 13:57:32 +0200

    Added replacer tests

---
+++ TDD/ReplacerTests.cs
@@ -0,0 +1,36 @@
+﻿namespace TDD
+{
+    using FluentAssertions;
+    using StringCalculator.Implementations;
+    using Xunit;
+
+    public class ReplacerTests
+    {
+        private Replacer sut;
+
+        public ReplacerTests()
+        {
+            this.sut = new Replacer();
+        }
+
+        ~ ReplacerTests()
+        {
+            this.sut = null;
+        }
+
+        [Theory,
+         InlineDataAttribute("1", "1"),
+         InlineDataAttribute("1 2", "1 2"),
+         InlineDataAttribute("1;3,-4\n9", "1 3 -4 9"),
+         InlineDataAttribute("1;3,-4\n-9", "1 3 -4 -9"),
+         InlineDataAttribute("1;3,-4\n-9 12 3.3",  "1 3 -4 -9 12 3.3"),
+         InlineDataAttribute("1,3;-4-9\n 12 3.3", "1 3 -4-9  12 3.3"),
+        ]
+        public void Validate(string value, string expected)
+        {
+            string result = this.sut.Replace(value);
+
+            result.Should().Be(expected);
+        }
+    }
+}
